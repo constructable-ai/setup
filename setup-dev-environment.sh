acc757fbe4df6502796d110f970ff1601c34ebc0
John Yoder <jcyoder@gmail.com>
2024-07-09 06:40:13 -0700

    Add missing line to start the nix daemon

--- setup-dev-environment.sh
+++ setup-dev-environment.sh
@@ -165,6 +165,7 @@ install_nix() {
   echo "Installing nix"
 
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
   
   echo "Done"
   echo
